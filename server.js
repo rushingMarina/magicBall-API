cab493e6214b793b916bb7249c64e520da175f1c
mordva42 <333marina2007@gmail.com>
2019-06-03 14:59:04 -0700

    Clarofai API is npw bwing fetched from the server instead of the front end

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
    client: 'pg',
@@ -21,8 +27,6 @@ const db = knex({
 const app = express();
 
 
-
-
 app.use(bodyParser.json());
 
 app.use(cors());
@@ -33,79 +37,16 @@ app.get('/', (req, res) => {
 })
 
 //signin, we using post in order to safly transfer info
-app.post('/signin', (req, res)=> {
-	db.select('email', 'hash').from('login')
-		.where('email','=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-			 return db.select('*').from('users')
-					.where('email','=', req.body.email)
-					.then(user => {
-						res.json(user[0])
-					})
-					.catch(err => res.status(400).json('unable to get user'))
-			} else
-			res.status(400).json('wrong credentials');
-		})
-		.catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 //register 
-app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
- 		let hash = bcrypt.hashSync(password, 8);
- 		db.transaction(trx => {
- 			trx.insert({
- 				hash: hash,
- 				email: email
- 			})
- 			.into('login')
- 			.returning('email')
- 			.then(loginEmail => {
- 				return trx('users')
-		    	.returning('*')
-		    	.insert({
-		    	email: loginEmail[0],
-		    	name: name, 
-		    	joined: new Date()
-		    	})
-		    	.then(user => {
-		    	res.json(user[0]);
-		    	})
- 			})
- 			.then(trx.commit)
- 			.catch(trx.rollback)
- 		})
-		.catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 // Profile, /:id means we can asccess it with .params
-app.get('/profile/:id', (req, res) => {
-	const {id} = req.params;
-	db.select('*').from('users').where({
-		id: id
-	})
-	.then(user => {
-		if (user.length){
-			res.json(user[0])
-		} else {
-			res.status(400).json('not found')
-		}
-	})
-	.catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-	const {id} = req.body;
-	db('users').where('id', '=', id)
-  	.increment('entries', 1)
-  	.returning('entries')
-  	.then(entries => {
-  		res.json(entries[0]);
-  	})
-  	.catch(err => res.status(400).json('unable to get count'))
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 
 
@@ -122,5 +63,4 @@ app.listen(3000, () => {
 /register --> POST = user
 /profile/:userId --> GET = user
 /image --> PUT --> user , increasing entries
-
 */
