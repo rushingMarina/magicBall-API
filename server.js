04c0da3c703fee7cbbe0208a8de30ba3dbe1fb61
mordva42 <333marina2007@gmail.com>
2019-06-06 18:00:47 -0700

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
    client: 'pg',
    connection: {
-    host : 'postgresql-animate-65889',
-    user : 'marina',
-    password : '',
-    database : 'magicball'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
